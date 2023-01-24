cd3e5e6de73f7494526e0c81a802c2370f430635
Tierry Barros <franciso.oliveira@dellead.com>
2023-01-24 20:10:17 -0300

    [main] - Update wal privacy policy

--- src/App.js
+++ src/App.js
@@ -3,30 +3,111 @@ import './App.css';
 
 function App() {
   return (
-    <table>
-      <caption>Alien football stars</caption>
-      <tr>
-        <th >*</th>
-        <th >a</th>
-        <th >b</th>
+    <>
+      <div>
+        <h1>POLÍTICA DE PRIVACIDADE</h1>
+      </div>
 
-      </tr>
-      <tr>
-        <th >1</th>
-        <td>7</td>
-        <td>4,569</td>
-      </tr>
-      <tr>
-        <th >2</th>
-        <td>7</td>
-        <td>7,223</td>
-      </tr>
-      <tr>
-        <th >3</th>
-        <td>9</td>
-        <td>6,219</td>
-      </tr>
-    </table>
+      <div id='conteudo'>
+        <p >O Centro de Pesquisa, Desenvolvimento e Inovação Dell (Lead) está comprometido em proteger sua privacidade e seus dados pessoais. Através dessa Política de Privacidade, descreveremos nossas práticas de privacidade e segurança implementadas na coleta, utilização e demais etapas do tratamento de informações pessoais na aplicação WAL.</p>
+        <p>Esta política também informa como você pode exercer seus direitos relativos a essas atividades de tratamento de dados pessoais. </p>
+        <p>O Lead poderá modificar a presente Política de Privacidade, mediante informativos divulgados no site da wal (<a href='http://leadfortaleza.com.br'>http://leadfortaleza.com.br</a>) para melhorá-la, adaptá-la ou atualizá-la a alterações legislativas ou jurisprudenciais.
+          Atenção: para que o usuário possa ter acesso aos benefícios da WAL, ele deverá ter ciência da presente Política de Privacidade e aceitar suas condições.
+        </p>
+
+        <ol>
+          <li>
+            <b>QUE DADOS SÃO COLETADOS E COMO?</b>
+            <ol>
+              <li>O Lead não irá coletar seus dados para armazenamento externo, seja no momento da instalação do aplicativo, seja durante o uso deste. Toda informação de uso, como dados cache, histórico de página acessadas, histórico de downloads, entre outros, ficarão armazenados unicamente em seu dispositivo, podendo ser apagados em qualquer momento de sua preferência.</li>
+            </ol>
+
+          </li>
+          <li>
+            <b>COMO OS DADOS SÃO UTILIZADOS?</b>
+            <ol>
+              <li>As informações coletadas durante o uso da aplicação poderão ser utilizados para os seguintes fins:
+                <ol>
+                  <li>Fornecer uma listagem de páginas acessadas durante a navegação para o histórico;</li>
+                  <li>Configurações de exibição do navegador para aprimorar a experiência, garantindo acessibilidade de acordo com a necessidade do usuário.</li>
+                </ol>
+              </li>
+            </ol>
+          </li>
+          <li>
+            <b>QUEM TEM ACESSO AOS DADOS?</b>
+            <ol>
+              <li>Os dados podem ser acessados apenas a partir do dispositivo do usuário. Não são acessados pelo Lead ou seus parceiros.</li>
+            </ol>
+          </li>
+          <li>
+            <b>POR QUANTO TEMPO MANTEREMOS SEUS DADOS?</b>
+            <ol>
+              <li>Nós não manteremos os seus Dados Pessoais, sendo eles preservados somente em seu dispositivo e sendo mantidos neste até que escolha deletá-los.</li>
+            </ol>
+          </li>
+          <li>
+            <b>COMO PROTEGEMOS SEUS DADOS?</b>
+            <ol>
+              <li>O Lead adota medidas técnicas e administrativas aptas a proteger os dados pessoais de acessos não autorizados e de situações acidentais ou ilícitas de destruição, perda, alteração, comunicação ou difusão. Apesar das medidas de segurança adotadas, nós não podemos garantir total segurança. O uso não autorizado de contas, falha de hardware ou software e outros fatores podem comprometer a segurança dos seus Dados Pessoais a qualquer momento.</li>
+              <li>As medidas de segurança tomadas buscam inibir e prevenir problemas que envolvam o tratamento dos dados fornecidos, ainda que não exista sistema 100% (cem por cento) seguro de proteção contra atuação de hackers ou terceiros munidos de métodos de obtenção de informações de forma indevida. Em razão disso, o Lead não se responsabiliza por acessos ilícitos, bem como por atos de terceiros que logrem êxito em coletar ou utilizar, por quaisquer meios, dados cadastrais e informações disponibilizadas na plataforma pelo usuário.</li>
+            </ol>
+          </li>
+
+          <li>
+            <b>COOKIES</b>
+            <ol>
+              <li>Os Cookies são arquivos criados pelos websites frequentados que registram as informações de navegação, possibilitando que o usuário se mantenha conectado, ao mesmo tempo que lembra suas preferências do site e fornece conteúdo direcionados.</li>
+              <li>O Lead poderá utilizar cookies para coletar informações do usuário e melhorar a experiência de navegação. Entretanto, caso o usuário queira rejeitar a instalação desses cookies, poderá bloqueá-los ou eliminá-los por meio do site <a href='http://tools.google.com/dlpage/gaoptout'>http://tools.google.com/dlpage/gaoptout</a> ou pelas próprias preferências do navegador.</li>
+            </ol>
+          </li>
+
+          <li>
+            <b>COMO EXERCER SEUS DIREITOS?</b>
+            <ol>
+              <li>No Brasil
+                A Lei Geral de Proteção de Dados Pessoais (LGPD), nº 13.709/2018, garante a você determinados direitos e garantias em relação aos seus dados pessoais.
+                O Lead, cumprindo com a sua obrigação de clareza e transparência, além de detalhar nessa política como utiliza e protege seus dados pessoais,
+                disponibiliza, <a href='https://leadfortaleza.com.br/dal/contato'>neste link</a>, um canal gratuito e facilitado para que você possa exercer seus direitos de:
+                <ol>
+                  <li>Solicitar Informações sobre o tratamento de seus dados pessoais: você pode confirmar se realizamos o tratamento de seus dados pessoais, requerer o acesso aos mesmos e correção de dados incorretos; além de solicitar a relação de todos os agentes com quem compartilhamos seus dados;</li>
+                  <li>Portabilidade: você tem o direito de solicitar a migração das suas informações pessoais para outra organização;</li>
+                  <li>Solicitar a exclusão ou anonimização de dados pessoais: você tem o direito de nos pedir a anonimização ou eliminação definitiva dos seus dados, caso eles se mostrem desnecessários ou excessivos para a finalidade do tratamento, ou caso estejam sendo tratados em desconformidade com a LGPD;</li>
+                  <li>Revogação do consentimento: você tem o direito de saber as consequências de recusar o fornecimento de consentimento para tratamento de suas informações pessoais e requerer a revogação do mesmo.</li>
+                  <li>Você também poderá entrar em contato com nosso encarregado de dados através do e-mail suporte@dellead.com.</li>
+                </ol>
+              </li>
+              <li>Na União Europeia
+                A Regulação Geral de Proteção de Dados da União Europeia (General Data Protection Regulation – GDPR) se aplica à coleta de dados de pessoas naturais que se
+                encontram na União Europeia, concedendo vários direitos relacionados ao tratamento de suas informações pessoais. Esses direitos estão abaixo relacionados e
+                podem ser exercidos através <a href='https://leadfortaleza.com.br/dal/contato'>deste link</a> ou entrando em contato com nosso DPO diretamente em suporte@dellead.com:
+                <ol>
+                  <li>Direito de acesso: solicitar informações sobre o tratamento de seus dados pessoais;</li>
+                  <li>Direito de retificação: solicitar a retificação de qualquer informação pessoal sua;</li>
+                  <li>Direito de exclusão: solicitar que seja apagado qualquer informação pessoal sua de nossa base de dados e de nossos parceiros;</li>
+                  <li>Direito de restrição: solicitar a restrição do processamento de informações pessoais que mantemos sobre você;</li>
+                  <li>Direito de portabilidade: você pode solicitar o recebimento de suas informações pessoais em um formato estruturado e legível. Você também tem o direito de exigir que a gente transfira essas informações pessoais para outra organização;</li>
+                  <li>Direito de oposição: você pode contestar nosso processamento das informações pessoais que mantemos sobre você;</li>
+                  <li>Direito de revogação de consentimento: você pode revogar o consentimento para utilização de seus dados pessoais.</li>
+                </ol>
+              </li>
+            </ol>
+          </li>
+
+          <li>
+            <b>CONTATO E DÚVIDAS</b>
+            <p>Caso tenha alguma dúvida a respeito do cumprimento da presente Política de Privacidade, você deverá entrar em contato com nosso encarregado, através do e-mail abaixo.</p>
+            <b>BARRETO E MAIA ADVOGADOS <br /></b>
+            <b>E-MAIL: privacy@barretoemaia.com.br <br /></b>
+            <b>Endereço: Avenida Santos Dumont, 1.510, 14º andar, Fortaleza/CE.</b>
+
+          </li>
+        </ol>
+
+      </div >
+
+    </>
+
   );
 }
 
